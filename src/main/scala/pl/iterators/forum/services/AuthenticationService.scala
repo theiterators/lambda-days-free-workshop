da6a4a5c53066199c8be0cbb362221b45ff689b8
Marcin Rzeźnicki <mrzeznicki@iterato.rs>
2018-02-24 12:49:46 +0100

    It wasn't that hard to add authentication logic

--- src/main/scala/pl/iterators/forum/services/AuthenticationService.scala
+++ src/main/scala/pl/iterators/forum/services/AuthenticationService.scala
@@ -3,12 +3,21 @@ package pl.iterators.forum.services
 import pl.iterators.forum.domain._
 import pl.iterators.forum.repositories._
 
+import pl.iterators.forum.utils.free.syntax._
+
 trait AuthenticationService {
   import AuthenticationService.AuthRequest
 
   type AuthenticationResponse = Either[AuthenticationError with Product with Serializable, Claims]
 
-  def authenticate(authRequest: AuthRequest): AccountOperation[AuthenticationResponse] = ???
+  def authenticate(authRequest: AuthRequest): AccountOperation[AuthenticationResponse] =
+    AccountRepository
+      .queryConfirmed(authRequest.email)
+      .toEither(ifNone = InvalidCredentials)
+      .subflatMap(subject => Either.cond(test = subject.canLogin, subject, if (subject.isBanned) Banned else InvalidCredentials))
+      .subflatMap(subject => Either.cond(test = subject.validatePassword(authRequest.password), subject, InvalidCredentials))
+      .map(_.claims)
+      .value
 
 }
 
