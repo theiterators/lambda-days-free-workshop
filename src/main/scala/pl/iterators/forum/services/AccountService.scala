36959dc91e1a29894b8b9ea7ed7bd76f293f48e0
Marcin Rzeźnicki <mrzeznicki@iterato.rs>
2018-02-24 11:53:40 +0100

    Tests are fixed!

--- src/main/scala/pl/iterators/forum/services/AccountService.scala
+++ src/main/scala/pl/iterators/forum/services/AccountService.scala
@@ -1,9 +1,9 @@
 package pl.iterators.forum.services
 
 import pl.iterators.forum.domain._
-import pl.iterators.forum.repositories.AccountOperation
+import pl.iterators.forum.repositories.{AccountOperation, AccountRepository}
 
 trait AccountService {
-  def queryEmail(email: Email): AccountOperation[Option[AccountWithId]] = ???
-  def lookup(id: AccountId): AccountOperation[Option[AccountWithId]]    = ???
+  def queryEmail(email: Email): AccountOperation[Option[AccountWithId]] = AccountRepository.queryEmail(email)
+  def lookup(id: AccountId): AccountOperation[Option[AccountWithId]]    = AccountRepository.lookup(id)
 }
